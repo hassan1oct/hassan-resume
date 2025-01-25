dfcd2e7d0c518d495b96eea70162906bf554321c
hassan1oct <hassan.khatri005@gmail.com>
2025-01-25 11:42:44 +0500

    test

--- pages/api/generate-pdf.js
+++ pages/api/generate-pdf.js
@@ -1,10 +1,14 @@
-import puppeteer from "puppeteer";
+import chromium from "chrome-aws-lambda";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     try {
-      const browser = await puppeteer.launch({ignoreDefaultArgs: ['--disable-extensions']})
-
+      const browser = await chromium.puppeteer.launch({
+        args: chromium.args,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath,
+        headless: chromium.headless,
+      });
       const page = await browser.newPage();
 
       // Tailwind CSS from CDN
